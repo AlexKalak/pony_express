d4b3c6e5e704c1244318b5546128e06024aa36dc
alexkalak <alexkalak2006@gmail.com>
2023-03-04 12:03:35 +0200

    test 3

--- src/Routes/api/countries/services/countries-service.go
+++ src/Routes/api/countries/services/countries-service.go
@@ -21,7 +21,7 @@ func (cs *countriesService) GetAllCountries(c *fiber.Ctx) (*[]models.Country, er
 	database := db.GetDB()
 
 	p_countries := new([]models.Country)
-	res := database.Joins("Cities.District").Joins("Cities.Area").Find(p_countries)
+	res := database.Preload("Cities.District").Preload("Cities.Area").Find(p_countries)
 	if res.Error != nil {
 		return nil, res.Error
 	}
